936a53e2632ff7510f864b59662f0fc30b96b006
Girey0211 <kkswin1234@gmail.com>
2024-05-17 23:07:17 +0900

    feat: swagger 도입

---
+++ src/util/swagger.ts
@@ -0,0 +1,14 @@
+import { INestApplication } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+export function setupSwagger(app: INestApplication): void {
+  const options = new DocumentBuilder()
+    .setTitle('SANDBURG assignment api reference')
+    .setDescription('김민석 SANDBURG 과제 api 명세서 - with swagger')
+    .setVersion('0.1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+}
