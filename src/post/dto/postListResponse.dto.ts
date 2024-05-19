155fe7a80d426e45712d11ac4f0dbb6de4cf0193
Girey0211 <kkswin1234@gmail.com>
2024-05-19 16:04:36 +0900

    feat: Dockerfile

--- src/post/dto/postListResponse.dto.ts
+++ src/post/dto/postListResponse.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEnum, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class PostListResponseDto {
   @ApiProperty({
